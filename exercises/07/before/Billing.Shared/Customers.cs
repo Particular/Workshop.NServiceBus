532e11c58d79d47d4ecdb8f27227907cd9f79758
Dennis van der Stelt <dvdstelt@gmail.com>
2018-03-03 00:36:16 +0100

    Added before folder and exercise readme

---
+++ exercises/07/before/Billing.Shared/Customers.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Billing.Shared
+{
+    public static class Customers
+    {
+        public static List<int> GetStrategicCustomers()
+        {
+            return new List<int>
+            {
+                3,
+                4,
+                5
+            };
+        }
+    }
+}
