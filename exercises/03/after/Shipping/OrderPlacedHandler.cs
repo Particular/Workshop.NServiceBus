bf67d9a9aa59e1efdbbef57175d3a6a33fe8b792
lailabougria <laila.bougria@particular.net>
2022-02-21 11:23:17 +0100

    Move into separate file

--- exercises/03/after/Shipping/OrderPlacedHandler.cs
+++ exercises/03/after/Shipping/OrderPlacedHandler.cs
@@ -16,15 +16,4 @@ namespace Shipping
             return Task.CompletedTask;
         }
     }
-
-    public class OrderBilledHandler : IHandleMessages<OrderBilled>
-    {
-        static ILog log = LogManager.GetLogger<OrderBilledHandler>();
-
-        public Task Handle(OrderBilled message, IMessageHandlerContext context)
-        {
-            log.Info($"Received OrderBilled, OrderId = {message.OrderId} - Should we ship now?");
-            return Task.CompletedTask;
-        }
-    }
 }
