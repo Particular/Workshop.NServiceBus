f41fd72bf7874f21fa35f0185bcca82d8a3501fe
David Boike <david.boike@gmail.com>
2024-10-15 13:01:39 -0500

    Output path during builds

--- build.ps1
+++ build.ps1
@@ -1,5 +1,7 @@
 $baseDir = (Get-Item -Path ".\" -Verbose).FullName
 $items = Get-ChildItem -Path $baseDir -Include *.sln -Recurse
 foreach ($item in $items){
+    Write-Output "Building $($item.FullName)"
     dotnet build $item
+    Write-Output ""
 }
