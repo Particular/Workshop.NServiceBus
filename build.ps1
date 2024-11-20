2dd77598cfd90016c944329c30e49e1702ad4100
Andreas Öhlund <andreas.ohlund@particular.net>
2024-11-20 11:19:45 +0100

    Remove scripts

--- build.ps1
+++
@@ -1,7 +0,0 @@
-$baseDir = (Get-Item -Path ".\" -Verbose).FullName
-$items = Get-ChildItem -Path $baseDir -Include *.sln -Recurse
-foreach ($item in $items){
-    Write-Output "Building $($item.FullName)"
-    dotnet build $item
-    Write-Output ""
-}
