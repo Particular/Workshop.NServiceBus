a4e5c258a0723b5be386f216808f3f1a582b4b06
Andreas Öhlund <andreas.ohlund@particular.net>
2024-11-20 15:13:57 +0100

    Make no6 into a docker exercise

--- exercises/06/after/Messages/PlaceOrder.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Messages
-{
-    using NServiceBus;
-
-    public class PlaceOrder : ICommand
-    {
-        public string OrderId { get; set; }
-    }
-}
--- exercises/06/before/Billing/Program.cs
+++
@@ -1,7 +0,0 @@
-using NServiceBus.Logging;
-
-#pragma warning disable CS0219 // Variable is assigned but its value is never used
-var endpointName = "Billing";
-#pragma warning restore CS0219 // Variable is assigned but its value is never used
-
-var log = LogManager.GetLogger<Program>();
