4cbcbc5611b918cd000574ba622520eac6026472
Brandon Ording <bording@gmail.com>
2025-04-18 16:52:42 -0400

    Update project files

--- exercises/07/after/ClientUI/Program.cs
+++ exercises/07/after/ClientUI/Program.cs
@@ -1,68 +1,58 @@
-﻿using Shared.Configuration;
+﻿using System;
+using System.Threading.Tasks;
+using Messages.Commands;
+using NServiceBus;
+using NServiceBus.Logging;
+using Shared;
 
-namespace ClientUI
+ILog log = LogManager.GetLogger<Program>();
+Random rnd = new Random();
+
+Console.Title = "ClientUI";
+
+var endpointConfiguration = new EndpointConfiguration("ClientUI");
+endpointConfiguration.Configure(s =>
 {
-    using Messages.Commands;
-    using NServiceBus;
-    using NServiceBus.Logging;
-    using System;
-    using System.Threading.Tasks;
+    s.RouteToEndpoint(typeof(PlaceOrder), "Sales");
+});
 
-    internal class Program
+var endpointInstance = await Endpoint.Start(endpointConfiguration);
+
+await RunLoop(endpointInstance);
+
+await endpointInstance.Stop();
+
+async Task RunLoop(IEndpointInstance endpointInstance)
+{
+    log.Info("- Press 'P' to place an order");
+    log.Info("- Press 'Q' to quit.");
+
+    while (true)
     {
-        private static readonly ILog log = LogManager.GetLogger<Program>();
-        private static readonly Random rnd = new Random();
+        var key = Console.ReadKey();
 
-        private static async Task Main()
+        switch (key.Key)
         {
-            Console.Title = "ClientUI";
-
-            var endpointConfiguration = new EndpointConfiguration("ClientUI");
-            endpointConfiguration.Configure(s =>
-            {
-                s.RouteToEndpoint(typeof(PlaceOrder), "Sales");
-            });
-
-            var endpointInstance = await Endpoint.Start(endpointConfiguration);
-
-            await RunLoop(endpointInstance);
-
-            await endpointInstance.Stop();
-        }
-
-        private static async Task RunLoop(IEndpointInstance endpointInstance)
-        {
-            log.Info("- Press 'P' to place an order");
-            log.Info("- Press 'Q' to quit.");
-
-            while (true)
-            {
-                var key = Console.ReadKey();
-
-                switch (key.Key)
+            case ConsoleKey.P:
+                // Instantiate the command
+                var command = new PlaceOrder
                 {
-                    case ConsoleKey.P:
-                        // Instantiate the command
-                        var command = new PlaceOrder
-                        {
-                            CustomerId = rnd.Next(10),
-                            OrderId = Guid.NewGuid().ToString()
-                        };
+                    CustomerId = rnd.Next(10),
+                    OrderId = Guid.NewGuid().ToString()
+                };
 
-                        // Send the command
-                        log.Info(
-                            $"Sending PlaceOrder command, CustomerId = {command.CustomerId}, OrderId = {command.OrderId}");
-                        await endpointInstance.Send(command);
-                        break;
+                // Send the command
+                log.Info(
+                    $"Sending PlaceOrder command, CustomerId = {command.CustomerId}, OrderId = {command.OrderId}");
+                await endpointInstance.Send(command);
+                break;
 
-                    case ConsoleKey.Q:
-                        return;
+            case ConsoleKey.Q:
+                return;
 
-                    default:
-                        log.Info("Unknown input. Please try again.");
-                        break;
-                }
-            }
+            default:
+                log.Info("Unknown input. Please try again.");
+                break;
         }
     }
 }
